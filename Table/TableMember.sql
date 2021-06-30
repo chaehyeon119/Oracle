4fd8b3fb1efbd914373a97dc2487609b8c522642
chaehyeon119 <83809520+chaehyeon119@users.noreply.github.com>
2021-06-30 18:33:05 +0900

    Add files via upload

---
+++ Table/TableMember.sql
@@ -0,0 +1,12 @@
+drop table memberT01;
+
+create table memberT01(
+mem_name VARCHAR(20),
+mem_id VARCHAR(20),
+mem_pwd VARCHAR2(20),
+mem_email VARCHAR2(20),
+mem_phone VARCHAR2(20),
+mem_addr VARCHAR2(20)
+);
+
+select *from tab;
